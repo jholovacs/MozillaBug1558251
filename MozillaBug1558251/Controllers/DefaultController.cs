3db833ba89d851a0b39cb6bac72bfe4afa06884d
Jeremy Holovacs <jeremy.holovacs@erpmaestro.com>
2019-06-18 10:06:14 -0400

    initial commit

---
+++ MozillaBug1558251/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MozillaBug1558251.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DefaultController : ControllerBase
+	{
+		[HttpGet("~/")]
+		public IActionResult GetTheFile()
+		{
+			var file = System.IO.File.OpenRead("Content/1558251.xlsx");
+			return File(file, "application/vnd.ms-excel.sheet.macroenabled.12", "MyFile.xlsx");
+		}
+	}
+}
